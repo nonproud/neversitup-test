2b5061231df87c2a5cb4e2d98fc9d5a295875beb
Pawan Panphinit <panphinit@gmail.com>
2020-06-11 15:38:26 +0700

    Complete product api

--- index.js
+++ index.js
@@ -3,27 +3,21 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
 
-let products = new Map()
+let products = []
 let productId = 0
 
 let orders = []
 let orderId = 0
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
-
 app.get('/products', (req, res) => {
     res.set('Content-Type', 'application/json')
-    let keys = Array.from( products.keys() );
-    console.log(keys)
     res.send(JSON.stringify(products))
 })
 
 app.get('/products/:id', (req, res) => {
+    res.set('Content-Type', 'application/json')
     let product = products.find(x => x.id == req.params.id)
     if(product != undefined ){
-        res.set('Content-Type', 'application/json')
         res.status(200).send(JSON.stringify(product))
     } else {
         res.status(404).send("Not found")
@@ -31,7 +25,7 @@ app.get('/products/:id', (req, res) => {
 })
 
 app.post('/products', (req, res) => {
-    products[productId] = ({id: productId, name: req.body.name, details: req.body.details})
+    products.push({id: productId, name: req.body.name, details: req.body.details})
     productId++
     res.send(200)
 })
@@ -50,7 +44,9 @@ app.put('/products/:id', (req, res) => {
 app.delete('/products/:id', (req, res) => {
     let product = products.find(x => x.id == req.params.id)
     if(product != undefined ){
-        products.pop(x => x.id == req.params.id)
+        products = products.filter(function( p ) {
+            return p.id !== product.id;
+        });
         res.status(200).send()
     } else {
         res.status(404).send("Not found")
@@ -73,20 +69,16 @@ app.get('/orders/:id', (req, res) => {
 })
 
 app.post('/orders', (req, res) => {
-    res.send('hello world delete')
+    res.send('hello world post delete')
 })
 
 app.delete('/orders', (req, res) => {
-    res.send('hello world delete')
+    res.send('hello world orders delete')
 })
 
 app.listen(3000, () => {
     console.log("On PORT: 3000")
-    products[0] = ({id: 0, name: "Product0", details: "Details of product 0"})
-    products[1] = ({id: 1, name: "Product1", details: "Details of product 1"})
-    products[2] = ({id: 2, name: "Product2", details: "Details of product 2"})
-    products[3] = ({id: 3, name: "Product3", details: "Details of product 3"})
-    products[4] = ({id: 4, name: "Product4", details: "Details of product 4"})
-    products[5] = ({id: 5, name: "Product5", details: "Details of product 5"})
-    productId = 6
+    products.push({id: 0, name: "Product0", details: "Details of product 0"})
+    products.push({id: 1, name: "Product1", details: "Details of product 1"})
+    productId = 2
 })
